0739723859f2fd0ce1d2909fa98b301360a28275
Alexander Cantarero <alexc281667@gmail.com>
2026-02-28 01:23:01 -0600

    added seven entities and attributes to schema

--- schema.sql
+++ schema.sql
@@ -16,3 +16,80 @@ CREATE TABLE Ride (
   FOREIGN KEY (location_id) REFERENCES Location(location_id)
 );
 
+CREATE TABLE PassType (
+  pass_type_id     INT                          AUTO_INCREMENT                   NOT NULL,
+  name             ENUM('fast pass', 'food pass', 'parking pass', 'season pass') NOT NULL,
+  description      VARCHAR(100)                                                  NULL,
+
+  PRIMARY KEY (pass_type_id)
+);
+
+CREATE TABLE Pass (
+  pass_id                INT                    AUTO_INCREMENT                  NOT NULL,
+  pass_type_id           INT                                                    NOT NULL,
+  customer_id            INT                                                    NOT NULL,
+  purchase_date          DATE                                                   NOT NULL,
+  quantity_purchased     INT                                                    NOT NULL CHECK (quantity_purchased >= 1),
+  quantity_remaining     INT                                                    NOT NULL CHECK (quantity_remaining >= 0),
+  status                 ENUM('active', 'expired')                              NOT NULL,
+
+  PRIMARY KEY (pass_id),
+  FOREIGN KEY (pass_type_id) REFERENCES PassType(pass_type_id),
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+CREATE TABLE TicketType (
+  ticket_type_id   INT                        AUTO_INCREMENT                   NOT NULL,
+  name             ENUM('park entry', 'ride ticket')                           NOT NULL,
+  description      VARCHAR(200)                                                NULL,
+
+  PRIMARY KEY (ticket_type_id)
+);
+
+CREATE TABLE Ticket (
+  ticket_id        INT                        AUTO_INCREMENT                   NOT NULL,
+  ticket_type_id   INT                                                         NOT NULL,
+  customer_id      INT                                                         NOT NULL,
+  valid_date       DATE                                                        NOT NULL,
+  status           ENUM('valid', 'used', 'expired')                            NOT NULL,
+
+  PRIMARY KEY (ticket_id),
+  FOREIGN KEY (ticket_type_id) REFERENCES TicketType(ticket_type_id),
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+CREATE TABLE RidesVisited (
+  usage_id        INT                        AUTO_INCREMENT                   NOT NULL,
+  ride_id         INT                                                         NOT NULL,
+  visit_id        INT                                                         NOT NULL,
+  start_time      DATETIME                                                    NOT NULL,
+  used_fast_pass  BOOLEAN                                                     NOT NULL,
+
+  PRIMARY KEY (usage_id),
+  FOREIGN KEY (ride_id) REFERENCES Ride(ride_id),
+  FOREIGN KEY (visit_id) REFERENCES Visit(visit_id)
+);
+
+CREATE TABLE `Transaction` (
+  transaction_id   INT                      AUTO_INCREMENT                   NOT NULL,
+  account_id       INT                                                       NULL,
+  transaction_time DATE                                                      NOT NULL,
+  total_amount     DECIMAL(10,2)                                             NOT NULL CHECK (total_amount >= 0),
+  payment_method   ENUM('cash', 'card')                                      NOT NULL,
+  venue_id         INT                                                       NOT NULL,
+
+  PRIMARY KEY (transaction_id)
+  FOREIGN KEY (account_id) REFERENCES Account(account_id),
+  FOREIGN KEY (venue_id) REFERENCES Venue(venue_id)
+);
+
+CREATE TABLE TransactionItem (
+  transaction_item_id  INT                 AUTO_INCREMENT                    NOT NULL,
+  transaction_id       INT                                                   NOT NULL,
+  item_type            ENUM('ticket', 'pass', 'merch', 'food', 'other')      NOT NULL,
+  quantity             INT                                                   NOT NULL CHECK (quantity >= 1),
+  unit_price           DECIMAL(10,2)                                         NOT NULL CHECK (unit_price > 0),
+
+  PRIMARY KEY (transaction_item_id),
+  FOREIGN KEY (transaction_id) REFERENCES `Transaction`(transaction_id)
+);
