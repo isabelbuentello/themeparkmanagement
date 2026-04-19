1ff91cdec8c2091ab0b94e5ba1cb1c1ce5c4fe83
isabelbuentello <isabelbuentellosb@gmail.com>
2026-04-19 15:31:10 -0500

    fix import

--- frontend/src/pages/admin/MaintenanceDash.jsx
+++ frontend/src/pages/admin/MaintenanceDash.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../../styles/gm-dash.css'
-import Toast from '../components/Toast'
+import Toast from '../../components/Toast'
 
 function MaintenanceDash() {
   const navigate = useNavigate()
